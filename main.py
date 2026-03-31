311a4335b5dec4729ac0cd80759c729f7972e80e
jybowserjr <yearginjackson@gmail.com>
2026-03-31 13:06:47 -0400

    Added PME. Tweaked main.

--- main.py
+++ main.py
@@ -1,46 +1,52 @@
-''''''
+'''Create log file and loop through all listed scrapers.'''
 #Python modules
 import warnings
 import logging
+from pathlib import Path
 
 
-
-#/-----Setup-----/#
+#/------------------Setup-----------------/#
 
 #Supress warnings. (For debug purposes, comment this line.)
 warnings.filterwarnings('ignore')
 
+#Ensure log appears in correct directory so that individual scripts may append messages to it
+repository_dir = Path(__file__).parent
+
 #Configure logging settings
-logging.basicConfig(filename='log.log', level=logging.INFO, filemode="w", format="%(asctime)s - %(message)s")
-#message used to track progress/debug
+logging.basicConfig(filename=repository_dir/'log_main.log', level=logging.INFO, filemode="w", format="%(asctime)s - %(message)s")
+
+#Message used to track progress/debug
 message = ""
+
 
 #Dictionary of module:function (Note that the scripts.___ is important for python to find directory). Alphabetical Order.
 import_list = {
-    "scripts.amf":"scrape_amf",
-    "scripts.ap2":"scrape_ap2",
-    "scripts.ap3":"scrape_ap3",
-    "scripts.ap4":"scrape_ap4",
-    "scripts.ap7":"scrape_ap7",
-    "scripts.bpfbouw":"scrape_bpfbouw",
-    "scripts.bpl":"scrape_bpl",
-    "scripts.detailhandel":"scrape_detailhandel",
-    "scripts.kpa":"scrape_kpa",
+    # "scripts.amf":"scrape_amf",
+    # "scripts.ap2":"scrape_ap2",
+    # "scripts.ap3":"scrape_ap3",
+    # "scripts.ap4":"scrape_ap4",
+    # "scripts.ap7":"scrape_ap7",
+    # "scripts.bpfbouw":"scrape_bpfbouw",
+    # "scripts.bpl":"scrape_bpl",
+    # "scripts.detailhandel":"scrape_detailhandel",
+    # "scripts.kpa":"scrape_kpa",
     "scripts.nbim":"scrape_nbim",
-    "scripts.pka":"scrape_pka",
-    "scripts.pmt":"scrape_pmt",
-    "scripts.vervoer":"scrape_vervoer"
+    # "scripts.pka":"scrape_pka",
+    "scripts.pme":"scrape_pme"
+    # "scripts.pmt":"scrape_pmt",
+    # "scripts.vervoer":"scrape_vervoer"
     }
 
 
 
 
 
-#/-----Run Scripts-----/#
+#/----------------Run Scripts-----------------/#
 
-#Log
 logging.info(f"Begin scraping")
 
+#For entry in import list
 for x in import_list:
     #Import the module
     module_x=__import__(x, fromlist=[None])
